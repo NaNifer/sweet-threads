50993a97e0bd2d59b370fb17a117f8e6b4014af8
Nifer Kilakila <nifercritter@gmail.com>
2023-06-20 21:40:31 -0700

    Basic page structure begin

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "Hats"
+    },{
+      id: 2,
+      title: "Jackets"
+    }, {
+      id: 3,
+      title: "Sneakers"
+    }, {
+      id: 4,
+      title: "Mens"
+    }, {
+      id: 5,
+      title: "Womens"
+    },
+  ]
   return (
-   
-   <div className="categories-contatiner">
-    <div className="category-container">
-      <img/>
-      <div className="category-body-container">
-        <h2 className="category-name">Hats</h2>
-        <p className="shop-button">Shop Now</p>
-      </div>
+
+    <div className="categories-contatiner">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          {/* <img /> */}
+          <div className="category-body-container">
+            <h2 className="category-name">{title}</h2>
+            <p className="shop-button">Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
-   </div>
   );
 }
 
