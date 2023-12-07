daea51e80ccf384d8368c5a63356ea1db835987e
Nifer Kilakila <nifercritter@gmail.com>
2023-12-06 19:59:58 -0800

    converting routes for styled components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,21 +1,32 @@
-import { useContext, Fragment } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+
+import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
-import CategoryPreview from '../../components/category-preview/category-preview.component';
 
-const CategoriesPreview = () => {
+import { CategoryContainer, Title } from './category.styles';
+
+const Category = () => {
+  const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
 
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </CategoryContainer>
     </Fragment>
   );
 };
 
-export default CategoriesPreview;
+export default Category;
